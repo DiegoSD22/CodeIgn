a22496efb105f0dfc290f2b5cec963fa39c17f98
DiegoSD22 <39284176+DiegoSD22@users.noreply.github.com>
2019-01-11 12:40:21 -0600

    Update pdfs_view.php

--- application/views/pdfs_view.php
+++ application/views/pdfs_view.php
@@ -1,39 +1,51 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Generación de reportes con codeIgniter</title>
-</head>
-<body>
-<h2 style="text-align: center">Imprime tus localidades</h2>
-<form method="post" action="<?=base_url()?>index.php/pdfs/generar" />
-<table align="center">
-    <tr>
-        <td>
-            <select name="provincia" id="provincia">
-     <option value="">Selecciona tu provincía</option>
-     <?php
-     foreach($provincias as $fila)
-     {
-     ?>
-     <option value=<?=$fila->id?>><?=$fila->provincia?></option>
-     <?php
- }
-     ?>
-     </select>
-        </td>
-    </tr>
-    <tr>
-     <td align="center" colspan="7">
-     <hr />
-         <input type="submit" value="Crear PDF" title="Crear PDF" />
-        </td>
-    </tr>
-</table>
-</form>
-<form enctype="multipart/form-data" action="<?=base_url()?>index.php/pdfs/upload" method="POST">
-<input name="uploadedfile" type="file" />
-<input type="submit" value="Subir archivo" />
-</form>
-</body>
-</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Generación de reportes con codeIgniter</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('sources/css2/bootstrap.min.css') ?>">
+            
+                </head>
+                <body>
+                    <div class="container">
+                        <h2 style="text-align: center">Imprime tus localidades</h2>
+                        <form method="post" action="<?= base_url() ?>index.php/pdfs/generar" />
+                        <table align="center">
+                            <tr>
+                                <td>
+                                    <select class="btn btn-outline-info"name="provincia" id="provincia">
+                                        <option value="">Selecciona tu provincía</option>
+                                        <?php
+                                        foreach ($provincias as $fila) {
+                                            ?>
+                                            <option value=<?= $fila->id ?>><?= $fila->provincia ?></option>
+                                            <?php
+                                        }
+                                        ?>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center" colspan="7">
+                                    <hr />
+                                    <input class="btn btn-outline-danger"type="submit" value="Crear PDF" title="Crear PDF" />
+                                </td>
+                            </tr>
+                        </table>
+                        </form>
+                    </div>
+
+                    <hr class="featurette-divider">
+
+                        <div class="container">
+                            <div class="row featurette">
+                                <div class="col-md-7">
+                                    <form enctype="multipart/form-data" action="<?= base_url() ?>index.php/pdfs/upload" method="POST">
+                                        <input name="uploadedfile" type="file" />
+                                        <input class="btn btn-outline-dark" type="submit" value="Subir archivo" />
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                </body>
+                </html>
